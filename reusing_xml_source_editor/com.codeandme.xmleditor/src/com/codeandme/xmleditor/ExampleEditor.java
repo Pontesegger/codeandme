7a2629614e7bc398c964cb27e46615b7ed202053
Christian Pontesegger <christian.pontesegger@web.de>
2015-05-12 12:50:57 +0200

    adding project: Reusing XML source editor

---
+++ reusing_xml_source_editor/com.codeandme.xmleditor/src/com/codeandme/xmleditor/ExampleEditor.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Christian Pontesegger
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christian Pontesegger - initial implementation
+ *******************************************************************************/
+package com.codeandme.xmleditor;
+
+import org.eclipse.wst.sse.ui.StructuredTextEditor;
+
+public class ExampleEditor extends StructuredTextEditor {
+
+	public ExampleEditor() {
+	}
+}
