61c0453a1768c8451e6b9002c68195431342782d
Christian Pontesegger <christian.pontesegger@web.de>
2015-07-30 15:22:34 +0200

    added tycho tutorial 05

---
+++ tycho/05_build_update_sites/com.codeandme.tycho.plugin/src/com/example/tycho/plugin/OpenViewCommand.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Christian Pontesegger
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christian Pontesegger - initial implementation
+ *******************************************************************************/
+package com.example.tycho.plugin;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class OpenViewCommand extends AbstractHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		try {
+			HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().showView(ExampleView.VIEW_ID);
+		} catch (PartInitException e) {
+		}
+
+		return null;
+	}
+}
