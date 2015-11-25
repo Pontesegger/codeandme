7924a6aa856ee21855ad5269a805e313ff476713
Christian Pontesegger <christian.pontesegger@web.de>
2015-11-25 16:19:39 +0100

    add beanshell basic interpreter

---
+++ ease_interpreter/02_basic_interpreter/org.eclipse.ease.lang.beanshell/src/org/eclipse/ease/lang/beanshell/IPluginConstants.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Christian Pontesegger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christian Pontesegger - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ease.lang.beanshell;
+
+public interface IPluginConstants {
+	String PLUGIN_ID = "org.eclipse.ease.lang.beanshell";
+}
