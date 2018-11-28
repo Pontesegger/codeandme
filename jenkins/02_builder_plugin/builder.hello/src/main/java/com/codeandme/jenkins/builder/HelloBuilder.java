34c4c5a8d29f1691dd2f7ab9de48101d54a8a24f
Christian Pontesegger <christian.pontesegger@web.de>
2018-11-28 09:31:22 +0100

    jenkins 02 tutorial code

--- jenkins/02_builder_plugin/builder.hello/src/main/java/com/codeandme/jenkins/builder/HelloBuilder.java
+++ jenkins/02_builder_plugin/builder.hello/src/main/java/com/codeandme/jenkins/builder/HelloBuilder.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import hudson.AbortException;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
@@ -17,19 +18,29 @@ import jenkins.tasks.SimpleBuildStep;
 
 public class HelloBuilder extends Builder implements SimpleBuildStep {
 
+	private boolean fFailBuild;
+
 	@DataBoundConstructor
-	public HelloBuilder() {
+	public HelloBuilder(boolean failBuild) {
+		fFailBuild = failBuild;
 	}
 
 	@Override
 	public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener)
 			throws InterruptedException, IOException {
 		listener.getLogger().println("This is the Hello plugin!");
+
+		if (isFailBuild())
+			throw new AbortException("Build error forced by plugin settings");
+	}
+
+	public boolean isFailBuild() {
+		return fFailBuild;
 	}
 
 	@Symbol("greet")
 	@Extension
-	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+	public static final class Descriptor extends BuildStepDescriptor<Builder> {
 
 		@Override
 		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
